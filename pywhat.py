cfb9c0b1b2f8bc9e1423ea37a5bde4ccae7846d7
RDevi2002 <113709406+RDevi2002@users.noreply.github.com>
2023-07-11 20:49:35 +0530

    Add files via upload

---
+++ pywhat.py
@@ -0,0 +1,13 @@
+
+import pywhatkit as py
+txt="Enter a msg  :"
+
+for i in range(2,4):
+
+    py.sendwhatmsg("+918897671456",txt,20,49)
+    py.playonyt("html tutorial")
+    py.search("python interview questions")
+
+
+
+
